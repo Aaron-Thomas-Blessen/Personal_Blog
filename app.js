60b3bb295a506edd8853f2d48cc087c033b93ed4
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-08-18 12:30:31 +0530

    database post.js created

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 // Initialize the express app
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
 
 app.use(express.static("public"));
 
