8cc78dba1990197efce809bbe57624b9803f656c
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-08-21 23:34:13 +0530

    admin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
-const adminLayout = '../views/layouts/admin';
+const adminLayout = "../views/layouts/admin";
 
 // Routes
 // GET /
 // Admin- Login Page
-router.get('/admin', async (req, res) => {
-    try {
-      const locals = {
-        title: "Admin",
-        description: "Simple Blog created with NodeJs, Express & MongoDb."
-      }
-  
-      res.render('admin/index', { locals, layout: adminLayout });
-    } catch (error) {
-      console.log(error);
-    }
-  });
+router.get("/admin", async (req, res) => {
+  try {
+    const locals = {
+      title: "Admin",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
 
+    res.render("admin/index", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-
-
-
+// Routes
+// GET /
+// Admin- Check Login
 
 module.exports = router;
